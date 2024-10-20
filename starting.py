be0e419d744485fa5ab0a30c0ea51dce8f77e7ee
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2024-10-20 16:52:31 -0300

    Add files via upload

---
+++ starting.py
@@ -0,0 +1,12 @@
+burger = 5
+fries = 6
+coke = 7
+
+if burger + fries < fries + coke:
+    print("Id like to have those every single day")
+
+elif burger - fries > coke + burger:
+    print("I wouldn't enjoy live as I do if I couldn't have one of those")
+
+
+
