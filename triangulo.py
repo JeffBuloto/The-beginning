255bc67ab6e75da3a76a7180e643ff5b07f9e34b
JeffBuloto <114021308+JeffBuloto@users.noreply.github.com>
2026-02-02 15:42:03 -0300

    Add files via upload

---
+++ triangulo.py
@@ -0,0 +1,6 @@
+B = int(input('Digite o valor da base do terreno: '))
+H = int(input('Digite o valor da altura do terreno: '))
+
+Area = ((B * H) /2)
+
+print('A área do terreno é:', Area, 'metros')
